32da36b31b0cb9e8d34085ad3fec865529a8e7b1
PauSGracia <pau.sole@gracia.lasalle.cat>
2026-03-12 20:01:35 +0100

    chore: add implementations

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,10 +1,20 @@
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
 }
 
+// El següent codi serveix per tal de que carregui les variables de secrets.properties
+val secretsFile = project.rootProject.file("app/secrets.properties")
+val secrets = Properties()
+if (secretsFile.exists()) {
+    secrets.load(secretsFile.inputStream())
+}
+
 android {
+
     namespace = "com.example.apiscript"
     compileSdk {
         version = release(36)
@@ -18,6 +28,8 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        buildConfigField("String", "API_KEY", "\"${secrets["API_KEY"] ?: ""}\"")
+        buildConfigField("String", "BASE_URL", "\"${secrets["BASE_URL"] ?: ""}\"")
     }
 
     buildTypes {
@@ -38,7 +50,14 @@ android {
     }
     buildFeatures {
         compose = true
+        // Aquesta variable per defecte té valor 'false', però li hem deposar
+        // 'true' per tal de que en l'acció de compilar
+        // incrusti les variables que hi ha definides a dins de
+        // secrets.properties a dins del projecte
+        buildConfig = true
+
     }
+
 }
 
 dependencies {
@@ -57,4 +76,32 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+//LIVEDATA
+    implementation("androidx.compose.runtime:runtime-livedata:1.7.5")
+//RETROFIT
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.9.3")
+//CORRUTINES
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+//GLIDE
+    implementation("com.github.bumptech.glide:compose:1.0.0-beta01")
+    testImplementation("junit:junit:4.13.2")
+//TESTS
+    testImplementation("io.mockk:mockk:1.12.2")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.10.01"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation("androidx.activity:activity-compose:1.9.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.0")
+// Retrofit + Gson
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("androidx.compose.material3:material3:1.3.0")
+    implementation("androidx.compose.material:material-icons-extended:1.7.5")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.6")
 }
