d61ac7befa4bf12698ff325fa575f5c25883833f
PauSGracia <pau.sole@gracia.lasalle.cat>
2026-03-13 18:13:40 +0100

    feat: create api files

---
+++ app/src/main/java/com/example/apiscript/api/AssistenciaApiService.kt
@@ -0,0 +1,33 @@
+package com.example.apiscript.api
+
+import com.example.apiscript.model.GetResponse
+import com.example.apiscript.model.Laptop
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+/**
+ * Interface que declara la signatura dels mètodes
+ * referents als endpoints de la API de JS.
+ */
+interface AssistenciaApiService {
+    /**
+     * GET totes les dades de la pestanya "Full 1" del Google Sheets
+     * @author RIS
+     */
+    @GET("exec")
+    suspend fun getDadesLaptop(
+        @Query("apiKey") apiKey: String, // Envia la API_KEY que consta a secrets.properties
+        // A través d'un atribut com 'type' podem atacar a diferents endpoints de tipus GET. Només cal capturar l'atribut e.parameter.type
+        //      des de la funció doGet(e) de JS i respondre amb les dades desitjades.
+        @Query("type") type: String = "laptop"
+    ): GetResponse<List<Laptop>>
+
+    /**
+     * POST per registrar entrada/sortida
+     * @author RIS
+     */
+//    @POST("exec")
+//    suspend fun inserirFila(
+//        @Body body: PostRequest
+//    ): PostResponse
+}
---
+++ app/src/main/java/com/example/apiscript/api/RetrofitInstance.kt
@@ -0,0 +1,29 @@
+package com.example.apiscript.api
+
+/* Per tal de poder importar BuildConfig, serà necessari fer:
+    1. Editar app/build.gradle.kts i afegir:
+        1.1. secretsFile = project.rootProject.file("app/secrets.properties") ...
+        1.2. buildConfigFields... dins de defaultConfig
+        1.3. buildFeatures{ buildConfig = true }
+    2. File -> Sync Project with Gradle Files
+    3. Build -> Clean Project
+    4. Build -> Assemble Module '...'
+ */
+
+import com.example.apiscript.BuildConfig
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import kotlin.jvm.java
+
+object RetrofitInstance {
+    // Agafa la BASE_URL de les peticions del servei web de secrets.properties
+    private val BASE_URL = BuildConfig.BASE_URL
+
+    val api: AssistenciaApiService by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(AssistenciaApiService::class.java)
+    }
+}
