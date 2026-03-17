94694d80d5d770dba5ea42da6d19f3fbdef43848
Marcel Roquet <marcel.roquet@gracia.lasalle.cat>
2026-03-17 21:49:28 +0100

    feat: implementar navHost

--- app/src/main/java/com/example/apiscript/nav/EntryPoint.kt
+++ app/src/main/java/com/example/apiscript/nav/EntryPoint.kt
@@ -2,14 +2,31 @@ package com.example.apiscript.nav
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.apiscript.view.LaptopScreen
+import com.example.apiscript.view.LogInScreen
+import com.example.apiscript.view.RegisterScreen
 import com.example.apiscript.viewmodel.LaptopViewModel
 
 @Composable
-fun EntyPoint(
+fun EntryPoint(
     viewModel: LaptopViewModel,
     apiKey: String,
-    navController: NavHostController = rememberNavController()
+    navController: NavHostController
 ){
-
+    NavHost(
+        navController = navController,
+        startDestination = Routes.RegisterScreen.route
+    ){
+        composable(Routes.LaptopScreen.route) {
+            LaptopScreen(viewModel = viewModel, apiKey = apiKey)
+        }
+        composable(Routes.LogInScreen.route) {
+            LogInScreen(viewModel,navController)
+        }
+        composable(Routes.RegisterScreen.route) {
+            RegisterScreen(viewModel = viewModel, navController = navController)
+        }
+    }
 }
