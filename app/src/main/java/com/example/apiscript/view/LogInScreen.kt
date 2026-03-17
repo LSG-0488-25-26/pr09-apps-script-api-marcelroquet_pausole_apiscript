63daab3ba9bf95f464d930be01482146b927bb1f
Marcel Roquet <marcel.roquet@gracia.lasalle.cat>
2026-03-17 22:03:22 +0100

    feat: creació bàsica de les vistes auth

--- app/src/main/java/com/example/apiscript/view/LogInScreen.kt
+++ app/src/main/java/com/example/apiscript/view/LogInScreen.kt
@@ -1,2 +1,99 @@
 package com.example.apiscript.view
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import com.example.apiscript.nav.Routes
+import com.example.apiscript.viewmodel.LaptopViewModel
+import okhttp3.Route
+
+@SuppressLint("UnrememberedMutableState")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LogInScreen( viewModel: LaptopViewModel,
+                 navController: NavController) {
+    val name = viewModel.nombreLog
+    val password = viewModel.passwordLog
+    var correct = true
+
+    Scaffold(
+        topBar = { TopAppBar(title = { Text("Login") }) }
+    ) { padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center
+        ) {
+
+            OutlinedTextField(
+                value = name,
+                onValueChange = { viewModel.updateNombreLog(it) },
+                label = { Text("Usuario") },
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            OutlinedTextField(
+                value = password,
+                onValueChange = { viewModel.updatePasswordLog(it) },
+                label = { Text("Contraseña") },
+                visualTransformation = PasswordVisualTransformation(),
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Button(
+                onClick = {
+                    if (viewModel.loginUsuer()) navController.navigate(Routes.LaptopScreen.route) else correct = false
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .border(
+                        width = 1.dp,
+                        color = if (!correct) Color.Red else Color.Transparent,
+                        shape = RoundedCornerShape(4.dp)
+                    )
+            ) {
+                Text("Login")
+            }
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Button(
+                onClick = { navController.navigate(Routes.RegisterScreen.route) },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text("Registrate")
+            }
+
+        }
+    }
+   }
--- app/src/main/java/com/example/apiscript/view/RegisterScreen.kt
+++ app/src/main/java/com/example/apiscript/view/RegisterScreen.kt
@@ -1,2 +1,96 @@
 package com.example.apiscript.view
 
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import com.example.apiscript.nav.Routes
+import com.example.apiscript.sharedPreferences.SettingsRepository
+import com.example.apiscript.viewmodel.LaptopViewModel
+import com.example.apiscript.viewmodel.LaptopViewModelFactory
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun RegisterScreen(
+    viewModel: LaptopViewModel,
+    navController: NavController
+) {
+    val name = viewModel.nombre
+    val password = viewModel.password
+    val correct = viewModel.isCorrect
+
+    Scaffold(
+        topBar = { TopAppBar(title = { Text("Registro") }) }
+    ) { padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center
+        ) {
+
+            OutlinedTextField(
+                value = name,
+                onValueChange = { viewModel.updateNombre(it) },
+                label = { Text("Usuario") },
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            OutlinedTextField(
+                value = password,
+                onValueChange = { viewModel.updatePassword(it) },
+                label = { Text("Contraseña") },
+                visualTransformation = PasswordVisualTransformation(),
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Button(
+                onClick = { viewModel.registrarUsuario() },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .border(
+                        width = 1.dp,
+                        color = if (!correct) Color.Red else Color.Transparent,
+                        shape = RoundedCornerShape(4.dp)
+                    )
+            ) {
+                Text("Registrar")
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(
+                onClick = { navController.navigate(Routes.LogInScreen.route) },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text("LogIn")
+            }
+        }
+    }
+}
