2923c3847e25cf0900b6b15505cb9b073440dff4
Marcel Roquet <marcel.roquet@gracia.lasalle.cat>
2026-03-17 21:58:50 +0100

    feat: implementar vmFactory i creació d'aquest en la main activity

--- app/src/main/java/com/example/apiscript/MainActivity.kt
+++ app/src/main/java/com/example/apiscript/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,33 +12,32 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.rememberNavController
+import com.example.apiscript.nav.EntryPoint
+import com.example.apiscript.sharedPreferences.SettingsRepository
 import com.example.apiscript.ui.theme.ApiScriptTheme
 import com.example.apiscript.view.LaptopScreen
+import com.example.apiscript.viewmodel.LaptopViewModel
+import com.example.apiscript.viewmodel.LaptopViewModelFactory
+import kotlin.getValue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        val repository = SettingsRepository(this, "user_prefs")
+        val viewModel: LaptopViewModel by viewModels {
+            LaptopViewModelFactory(repository)
+        }
         setContent {
-            LaptopScreen (
-                apiKey = BuildConfig.API_KEY
-            )
+            ApiScriptTheme {
+                val navController = rememberNavController()
+                EntryPoint(viewModel = viewModel, apiKey = BuildConfig.API_KEY, navController)
+            }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ApiScriptTheme {
-        Greeting("Android")
-    }
-}
+
--- app/src/main/java/com/example/apiscript/viewmodel/LaptopViewModelFactory.kt
+++ app/src/main/java/com/example/apiscript/viewmodel/LaptopViewModelFactory.kt
@@ -1,4 +1,16 @@
 package com.example.apiscript.viewmodel
 
-class LaptopViewModelFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.apiscript.sharedPreferences.SettingsRepository
+
+class LaptopViewModelFactory(private val repository: SettingsRepository) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(LaptopViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return LaptopViewModel(repository) as T
+        }
+        throw IllegalArgumentException("NO ES TROBA EL VM")
+    }
 }
